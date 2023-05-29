9538dc17e3be761e961ba5e38cadc21636b3ddab
Sean Culliton <smculliton@gmail.com>
2023-05-29 16:34:05 -0600

    Update Sides

--- data/side-limits.js
+++ data/side-limits.js
@@ -1,10 +1,13 @@
 const sideLimits = {
-  'Beans': 3,
+  'Beans': 4,
   'Pasta Salad': 5,
   'Potato Salad': 5,
   'Cole Slaw': 5,
-  'BBQ Sauce': 5,
-  'Charcuterie': 2
+  'BBQ Sauce': 3,
+  'Green Salad': 5,
+  'Charcuterie': 2,
+  'Beer': 5,
+
 }
 
 export default sideLimits
